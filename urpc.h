8122465b0303805822f907fdd0f1d8e4086823c9
Anastasya Myasnikova <nikipov@gmail.com>
2022-08-31 12:38:59 +0300

    version increment

--- urpc.h
+++ urpc.h
@@ -12,7 +12,7 @@ typedef int urpc_result_t;
 
 #define URPC_XINET_VERSION_MAJOR  1
 #define URPC_XINET_VERSION_MINOR  1
-#define URPC_XINET_VERSION_BUGFIX 0
+#define URPC_XINET_VERSION_BUGFIX 1
 
 #define URPC_CID_SIZE 4
 #define URPC_CRC_SIZE 2
